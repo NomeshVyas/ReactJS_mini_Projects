5ade7ab6e361d8ece700e6b3639ff64fbabb324c
NomeshVyas <nomesh9351962867@gmail.com>
2023-04-21 23:41:53 +0530

    adding weather informations

--- weather_app/src/Components/WeatherBox.js
+++ weather_app/src/Components/WeatherBox.js
@@ -8,41 +8,59 @@ function WeatherBox() {
    const [tempMax, setTempMax] = useState(null)
    const [cityName, setCityName] = useState(null)
    const [countryName, setCountryName] = useState(null)
+   const [windSpeed, setWindSpeed] = useState(null)
+   const [humidity, setHumidity] = useState(null)
+   const [weather, setWeather] = useState(null)
    
 
    const fetchData = async () => {
+      // if(!queryCity)return;
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?&q=${queryCity}&appid=${ApiKey}`);
-      const jsonData = await response.json()
+      const jsonData = await response.json();
       console.log(jsonData);
       setCityName(jsonData.name);
       setNormalTemp(jsonData.main.temp);
       setTempMin(jsonData.main.temp_min);
       setTempMax(jsonData.main.temp_max);
       setCountryName(jsonData.sys.country);
+      setWindSpeed(jsonData.wind.speed);
+      setHumidity(jsonData.main.humidity);
+      setWeather(jsonData.weather[0].description);
    }
 
    useEffect(() => {
       fetchData();
       // eslint-disable-next-line
    }, [queryCity])
-   
 
   return (
    <div className="screen">
       <div className="container">
-         <div className="input-box flex">
-            <input className='input' type="search" onChange={(e)=>{setQueryCity(e.target.value)}} />
-         </div>
-         <div className="city-name flex">
-         <i className="fa-solid fa-street-view"></i>{cityName} ({countryName})
-         </div>
-         <div className="temp">
-            <div className="normal-temp flex"> {(normalTemp-273.15).toFixed(2)}°С</div>
-            <div className="min-max flex">Min : {(tempMin-273.15).toFixed(2)}°С | Max : {(tempMax-273.15).toFixed(2)}°С</div>
-         </div>
+         <form className="input-box flex" onSubmit={(e)=>{e.preventDefault(); fetchData();}}>
+            <input className='input' placeholder='Enter Your City Name' defaultValue={"ajmer"} type="search" onChange={(e)=>{setQueryCity(e.target.value);}} />
+         </form>
+         {cityName && <div style={{height: "80%"}}>
+            <div className="city-name flex">
+            <i className="fa-solid fa-street-view"></i>{cityName} <span className='orange'>({countryName})</span>
+            </div>
+            <div className="temp">
+               <div className="normal-temp flex"> {(normalTemp-273.15).toFixed(2)}<span className="orange">°С</span></div>
+               <div className="min-max flex">Min : {(tempMin-273.15).toFixed(2)}<span className="orange">°С</span> <span style={{marginInline: "10px"}}>|</span> Max : {(tempMax-273.15).toFixed(2)}<span className="orange">°С</span></div>
+            </div>
+               <div className="weather flex" style={{fontSize: "1.1rem"}}>Weather: {weather} <span className='icon' style={{backgroundImage: "url(./weather_icon_full_sun.svg)"}}></span></div>
+            <div className="otherInformation">
+               <div style={{display: "flex", justifyContent: "space-between", marginInline: "10%"}}>
+                  <div className="humidity">Humidity: <span className="orange">{humidity}</span></div>
+                  <div className="windSpeed">Wind Speed: <span className="orange">{windSpeed}</span></div>
+               </div>
+            </div>
+         </div>}
+         {!cityName && <div className='flex notFound' style={{height: "80%", fontSize: "2.25rem"}}>
+            City not Found
+         </div>}
       </div>
    </div>
   )
 }
 
-export default WeatherBox
+export default WeatherBox;
